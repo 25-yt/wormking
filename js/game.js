8a90f6b4a49488f34c0846fc0efab17161d09b61
25-yt <abolgaming298@gmail.com>
2025-07-13 18:05:18 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -6519,7 +6519,7 @@ $('#default-cursor-btn').click(function () {
         id = name;
         
         $.ajax({
-            url: 'https://wormateserkanconnect.github.io/wormateserkanconnect6/api/skins.php',
+            url: 'https://25-yt.github.io/wormking/api/skins.php',
             method: 'GET',
             dataType: 'json',
             success: function (id) {
