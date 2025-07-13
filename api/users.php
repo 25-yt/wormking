1901ee9a3fe9585c4249c776f9fd367ff4b96603
25-yt <abolgaming298@gmail.com>
2025-07-13 16:39:39 +0200

    Update users.php

--- api/users.php
+++ api/users.php
@@ -12,7 +12,8 @@
             "status": 1,
             "Client_VisibleSkin": "1200",
             "Client_VisibleSkin1": "1202",
-            "Client_VisibleSkin1": "1201"
+            "Client_VisibleSkin1": "1201",
+            "Client_VisibleSkin1": "1202"
         },
         {
             "id": 2,
