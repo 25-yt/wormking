a83730fe0231fa9ee0531b006643596dd9a3941c
25-yt <abolgaming298@gmail.com>
2025-07-18 21:10:40 +0200

    Update users.php

--- api/users.php
+++ api/users.php
@@ -362,6 +362,25 @@
               "Client_VisibleSkin8": "0000",
               "Client_VisibleSkin9": "0000",
               "Client_VisibleSkin10": "0000"
+        },
+        {
+              "id": 21,
+              "cliente_NOMBRE": "tenenup",
+              "cliente_ID": "gg_101324750622109474079",
+              "Client_KeyAccecs": "XTPRIVATESKIN",
+              "cliente_DateExpired": "2027-05-04",
+              "status": 1,
+              "Client_VisibleSkin": "0000",
+              "Client_VisibleSkin1": "0000",
+              "Client_VisibleSkin2": "0000",
+              "Client_VisibleSkin3": "0000",
+              "Client_VisibleSkin4": "0000",
+              "Client_VisibleSkin5": "0000",
+              "Client_VisibleSkin6": "0000",
+              "Client_VisibleSkin7": "0000",
+              "Client_VisibleSkin8": "0000",
+              "Client_VisibleSkin9": "0000",
+              "Client_VisibleSkin10": "0000"
         }
         
         
